857375fab69abe215fa5fe3290205e2fac24504a
h3hota-aabb <34065143+h3hota-aabb@users.noreply.github.com>
2017-12-18 16:08:42 +0300

    Add files via upload

--- news/newsimage.js
+++ news/newsimage.js
@@ -1,7 +1,10 @@
 function openImg(id, src){
-	var img = document.getElementById(id);
-	img.src = src;        
-	var w = img.naturalWidth;
-	var h = img.naturalHeight;
+	var smallimg = document.getElementById(id);  
+	var name = smallimg.src.substring( src.lastIndexOf("/") + 1 ); 
+	src = "bigimage/" + name;
+	var img = new Image();
+	img.src = src;
+	var w = name.substring( name.indexOf("(")+1, name.indexOf("x"));
+	var h = name.substring( name.indexOf("x")+1, name.indexOf(")"));
 	window.open(src,"none","width=" + w + ",height=" + h + ',left=' + ((screen.width - w)/2) + ',top=' + ((0.9*screen.height - h)/2));
 }
